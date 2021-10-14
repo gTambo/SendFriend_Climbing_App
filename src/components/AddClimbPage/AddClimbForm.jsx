fc2dfcbf826b816b36f618c1a948276d09e24e6a
Greg Tambo <gregtambornino@gmail.com>
2021-10-14 00:32:10 -0500

    start of add climb form

--- src/components/AddClimbPage/AddClimbForm.jsx
+++ src/components/AddClimbPage/AddClimbForm.jsx
@@ -1,14 +1,24 @@
-
+import React, { useState } from 'react';
 
 function AddClimbForm() {
 
+    const [grade, setGrade] = useState();
+
+    // to 
+
     return(
         <form>
-            <label htmlFor=""></label> 
-            <input htmlId="" type="text" />
+            <label htmlFor="grade">Grade</label> 
+            <input id="grade" 
+                   type="text" 
+                   value={grade} 
+                   onChange={ (event) => setGrade(event.target.value)}
+            />
             <input type="text" />
             <input type="text" />
 
         </form>
     )
 }
+
+export default AddClimbForm;
--- src/components/AddClimbPage/AddClimbPage.jsx
+++ src/components/AddClimbPage/AddClimbPage.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import AddClimbForm from './AddClimbForm';
 
 function AddNewClimb() {
     const history = useHistory();
@@ -13,6 +14,7 @@ function AddNewClimb() {
             Climblist:{JSON.stringify(climbList)}
             Params: {JSON.stringify(allParams)}
             <p>add new climb here!</p>
+            <AddClimbForm />
             <button onClick={ () => history.goBack() }>Back</button>
         </div>
     )
