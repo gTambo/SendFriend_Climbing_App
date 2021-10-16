01d1701a3103acc63e8959164d6fd268f33086cd
Greg Tambo <gregtambornino@gmail.com>
2021-10-15 20:07:43 -0500

    reinstalled updated MUI, imported rating into DetailsPage

--- src/components/ClimbDetail/ClimbDetail.jsx
+++ src/components/ClimbDetail/ClimbDetail.jsx
@@ -3,13 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 const moment = require('moment');
 
+// MUI styles here
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
 function ClimbDetail() {
+    const [value, setValue] = useState(4);
     // use dispatch to get details
-        const dispatch = useDispatch();
-        const history = useHistory();
-        //  use Selector to get details from redux
-        const climbDetails = useSelector(store => store.climbDetails);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    //  use Selector to get details from redux
+    const climbDetails = useSelector(store => store.climbDetails);
     // use params to get climb ID
     const { gymId, climbId, styleId } = useParams();
     const addDate = moment(climbDetails.date_added).format("dddd, MMMM, do YYYY");
@@ -47,7 +52,18 @@ function ClimbDetail() {
             <p>Color: {climbDetails.color}</p>
             <p>Date added {addDate}</p>
             <p>Movement style: {climbDetails.movement_style}</p>
-
+            {/* <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                }}
+                >
+                <Typography component="legend">Rating</Typography>
+                <Rating
+                    name="read-only"
+                    max={4}
+                    value={4}
+                />
+            </Box> */}
             <button onClick={ logRoute }>Log Your Send</button>
             {/* <button onClick={ confirmDelete } >Delete Climb</button> */}
             <button onClick={ toEditPage } >Edit Climb</button>
